f49eb54b0d848bbe14fcff75425ae8cf876bd073
Augusto Bizarria <agbiza1919@gmail.com>
2024-08-19 12:31:51 -0300

    palin rec c

--- TPs/TP01/Q11/main.c
+++ TPs/TP01/Q11/main.c
@@ -1,66 +1,40 @@
-//Palindromo Recursivo
 #include <stdio.h>
-#include <string.h>
 #include <stdbool.h>
+#include <string.h>
 
-bool verificar(char *texto, char *copia, int tam, int z) {
-    
-    bool palin = true;
-
-    if(tam == 1) {
-        palin = true;
-    }
-
-    if(z == '\0') {
-        
-    }
-
-    else {
-        if(texto[z] == copia[z]) verificar(texto,copia,tam,z+1);
-        else palin = false;
-    }
-
-    return palin;
-}
-
-void inverter(char *copia, int tam) {
-    int i = 0, f = tam - 1, aux;
-
-    while(i <= f) {
-        aux = copia[i];
-        copia[i] = copia[f];
-        copia[f] = aux;
-
-        i++; f--;
-    }
-}
-
-bool palindromo(char *texto) {
+bool palindromo(char *texto, int tam) {
     bool palin = false;
-
-    int c = strlen(texto);
-
-    char copia[1000];
-    strcpy(copia, texto);
-    inverter(copia, c);
-
-    if(verificar(texto, copia, c-1, 0)) palin = true;
-
+    int c = 0;
+    int i = 0, f = tam;
+    if(tam == 0) {
+        c++;
+    }
+    else {
+        if(texto[f] != texto[i]);
+        else c++;
+        palindromo(texto, tam-1);
+    }
+    
+    if(c > 0) palin = true;
     return palin;
 }
+
 
 
 int main(void) {
 
     char texto[1000];
-    do{
-        scanf(" %[^/n]", texto);
 
+    do{
+        scanf(" %[^\n]", texto);
         if(strcmp(texto, "FIM") != 0) {
-            if(palindromo(texto)) printf("SIM\n");
+            int n = strlen(texto)-1;
+            int x = palindromo(texto, n);
+            if(x) printf("SIM\n");
             else printf("NAO\n");
         }
+    }while(strcmp(texto, "FIM") != 0);
 
-    } while(strcmp(texto, "FIM") != 0);
 
+    return 0;
 }
