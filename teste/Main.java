2af7dd580363f2a617f9965042720e0c21ce3053
Augusto Bizarria <agbiza1919@gmail.com>
2025-02-18 17:42:00 -0300

    cifra cesar falta tratar char unicode

--- teste/Main.java
+++ teste/Main.java
@@ -10,7 +10,7 @@ class Main {
         for(int i = 0; i < tam2; i++) {
             str2[i] = str.charAt(i);            //copia a string para um char array
 
-            str2[i] = (char)(str2[i] + 3);      
+            str2[i] += 3;                       //alterando o unicode da tabela ascci, logo altera-se o caractere em 3 pra frente
         }
 
         String strCesar = new String (str2);    //transforma o char array em string
