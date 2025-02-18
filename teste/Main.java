3263102a8c5dd98ea1a6b5e9dd7a7b2f55f65b09
Augusto Bizarria <agbiza1919@gmail.com>
2025-02-18 15:09:10 -0300

    cifrmaneto de cesar faltando comentarios

---
+++ teste/Main.java
@@ -0,0 +1,44 @@
+//Ciframento de Cesar em Java (interativo)
+import java.util.Scanner;
+class Main {
+
+    public static String ciframentoCesar(String str, int tam) {
+
+        char[] str2 = new char[tam];        
+        int tam2 = str2.length;
+
+        for(int i = 0; i < tam2; i++) {
+            str2[i] = str.charAt(i);            //copia a string para um char array
+
+            str2[i] = (char)(str2[i] + 3);      
+        }
+
+        String strCesar = new String (str2);    //transforma o char array em string
+
+        return strCesar;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = new String();
+
+        do{
+
+            str = scan.nextLine();
+            int tam = str.length();
+
+            if(!str.equals("FIM")) {
+
+                System.out.println(ciframentoCesar(str, tam));
+            }
+
+        }while(!str.equals("FIM"));
+
+
+
+        scan.close();
+    }
+}
