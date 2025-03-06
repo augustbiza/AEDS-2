07f6f6a31fb733e639394d42c10ca3d1c5a85968
Augusto Bizarria <agbiza1919@gmail.com>
2025-03-06 14:49:15 -0300

    att .equals(fim)

--- TP/funcoes_e_macetes/funcoes-Java/String/equals(FIM).java
+++ TP/funcoes_e_macetes/funcoes-Java/String/equals(FIM).java
@@ -6,7 +6,7 @@ public static boolean comparaFim(String str, int tam) {     //str.equals("FIM")
         if(str.charAt(0) == 'F' && str.charAt(1) == 'I' && str.charAt(tam-1) == 'M') igualFim = true;   //tam-1 = ultimo char, em java não possui \0
 
         return igualFim;
-    }
+}
 
 public static void main(String[] args) {
 
